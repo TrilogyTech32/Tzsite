aa6c5ccd51c007350893ff2f70af7594c701e350
Campana32 <gabriel.rcsantos@hotmail.com>
2024-07-03 23:04:54 -0300

    Primeiro commit

---
+++ src/App.js
@@ -0,0 +1,10 @@
+
+function App() {
+  return (
+    <div className="w-full h-screen bg-zinc-800 ">
+      <h1 className='text-6xl text-white'>Teste do TailwindCss</h1>
+    </div>
+  );
+}
+
+export default App;
